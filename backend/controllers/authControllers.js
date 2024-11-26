ad1181e404018ff034160d2131eb9b2e40734072
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-11-26 17:11:38 +0100

    Login API is added

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js"
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 export const register = async (req, res) => {
     const { fullName, email, password } = req.body
@@ -42,5 +43,27 @@ export const register = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-    
+    const { email, password } = req.body
+    try {
+        const user = await User.findOne({ email })
+
+        if(!user) {
+            return res.status(404).json({ message: "user is not found" })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" })
+        }
+
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "1h" }
+        )
+        res.status(200).json({ message: "Login Successful", email, token })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: `Something went wrong`})
+    }
 }
