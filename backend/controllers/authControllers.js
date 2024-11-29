a80548e9377b9d1f06607754e6b60c9890b82de8
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-11-29 09:44:54 +0100

    Get User API is added

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -68,3 +68,19 @@ export const login = async (req, res) => {
         res.status(500).json({message: `Something went wrong`})
     }
 }
+
+export const getUser = async (req, res) => {
+    const { user } = req;
+
+    const isUser = await User.findOne({ _id: user._id })
+    if (!isUser) {
+        return res.status(404).json({ message: "User not found" })
+    }
+
+    return res.status(200).json({ user: {
+        _id: isUser._id,
+        fullName: isUser.fullName,
+        email: isUser.email,
+        createdAt: isUser.createdOn
+    }, message: "" })
+}
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { login, register } from "../controllers/authControllers.js";
+import { getUser, login, register } from "../controllers/authControllers.js";
+import { authenticateToken } from "../middleware/authToken.js";
 
 const router = Router()
 
 router.post("/register", register)
 router.post("/login", login)
+router.get("/get-user", authenticateToken, getUser)
 
 export default router
